f85fc3033f1081f08fed8e706acb8436f5b7a5b8
zhao- <calcite@139.com>
2023-04-15 15:30:10 +0800

    表格迁移至element ui。

--- src/main/resources/static/js/indexUtils.js
+++ src/main/resources/static/js/indexUtils.js
@@ -49,3 +49,23 @@ export function verifyFollowList(list) {
 		verifyFollowItem(list[index]);
 	}
 }
+
+export function formatDate(unixTimestamp) {
+	console.log('unixTimestamp:' + unixTimestamp)
+	if (typeof unixTimestamp !== 'number' || unixTimestamp <= 0) {
+		return 'N/A';
+	}
+	const date = new Date(unixTimestamp * 1000);
+	const y = date.getFullYear();
+	let m = date.getMonth() + 1;
+	m = m < 10 ? ('0' + m) : m;
+	let d = date.getDate();
+	d = d < 10 ? ('0' + d) : d;
+	let h = date.getHours();
+	h = h < 10 ? ('0' + h) : h;
+	let minute = date.getMinutes();
+	minute = minute < 10 ? ('0' + minute) : minute;
+	let second = date.getSeconds();
+	second = second < 10 ? ('0' + second) : second;
+	return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+}
